953f3af9fd569ec1d526f4db87401e5b5e587ab6
KristiSeraj <serajkristi@gmail.com>
2023-04-26 10:41:23 +0200

    added new endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 import { getStatus, getStats } from '../controllers/AppController';
 import postNew from '../controllers/UsersController';
+import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
 
 const express = require('express');
 
 const router = express.Router();
 
 router.use(express.json());
+
 router.get('/status', getStatus);
 router.get('/stats', getStats);
 router.post('/users', postNew);
+router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.get('/users/me', getMe);
 
 export default router;
